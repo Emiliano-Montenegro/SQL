bcdd97dff8ea86e9af1bc1c3e64319f627938b88
Emiliano-Montenegro <montenegrol.emiliano@gmail.com>
2023-10-17 17:18:12 -0300

    SP

--- Stored-Montenegro.sql
+++ Stored-Montenegro.sql
@@ -1,27 +1,47 @@
 DELIMITER //
--- SP para ordenar una tabla por un campo específico en orden ascendente o descendente
-CREATE PROCEDURE OrdenarTabla(IN NombreTabla VARCHAR(255), IN NombreCampo VARCHAR(255), IN Orden VARCHAR(10))
+CREATE PROCEDURE spOrdenarTabla(
+    IN tablaNombre VARCHAR(50),
+    IN campoOrden VARCHAR(50),
+    IN orden INT
+)
 BEGIN
-    SET @query = CONCAT('SELECT * FROM ', NombreTabla, ' ORDER BY ', NombreCampo, ' ', Orden);
-    PREPARE stmt FROM @query;
+    SET @sql = CONCAT('SELECT * FROM ', tablaNombre, ' ORDER BY ', campoOrden, ' ', IF(orden = 1, 'ASC', 'DESC'));
+    PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
-END;
-//
+END //
 DELIMITER ;
 
--- SP para modificar una tabla (INSERT o DELETE)
+CALL spOrdenarTabla('MiTabla', 'CampoOrdenamiento', 1); -- Orden ascendente
+CALL spOrdenarTabla('MiTabla', 'CampoOrdenamiento', 0); -- Orden descendente
+
 DELIMITER //
-CREATE PROCEDURE ModificarTabla(IN Accion VARCHAR(10), IN NombreTabla VARCHAR(255), IN Valor1 VARCHAR(255), IN Valor2 VARCHAR(255))
+CREATE PROCEDURE spInsertarEliminarRegistro(
+    IN pTablaNombre VARCHAR(50),
+    IN pAccion INT,
+    IN pId INT
+)
 BEGIN
-    IF Accion = 'INSERT' THEN
-        SET @query = CONCAT('INSERT INTO ', NombreTabla, ' VALUES (', Valor1, ', ', Valor2, ')');
-        CALL runSQL(@query);  -- Llamamos a la función runSQL para ejecutar la consulta
-    ELSEIF Accion = 'DELETE' THEN
-        SET @query = CONCAT('DELETE FROM ', NombreTabla, ' WHERE columna1 = ', Valor1, ' AND columna2 = ', Valor2);
-        CALL runSQL(@query);  -- Llamamos a la función runSQL para ejecutar la consulta
+    IF pAccion = 1 THEN
+        -- Insertar un nuevo registro
+        SET @query = CONCAT('INSERT INTO ', pTablaNombre, ' (...) VALUES (...);'); -- Reemplaza los campos y valores apropiados
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    ELSEIF pAccion = 2 THEN
+        -- Eliminar un registro por ID
+        SET @query = CONCAT('DELETE FROM ', pTablaNombre, ' WHERE ID = ', pId, ';');
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
     END IF;
-END;
-//
+END //
 DELIMITER ;
+
+-- Para insertar un nuevo registro en la tabla
+CALL spInsertarEliminarRegistro('NombreDeLaTabla', 1, NULL);
+
+
+-- Para eliminar un registro por ID
+CALL spInsertarEliminarRegistro('NombreDeLaTabla', 2, ID_A_Eliminar);
 
